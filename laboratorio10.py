917cb89c3c134d6191c197ce2c422226aa50f70f
CarlosFCJ <62518018+CarlosFCJ@users.noreply.github.com>
2020-04-08 10:23:57 -0500

    Modificacion con multiples a de entrada

--- laboratorio10.py
+++ laboratorio10.py
@@ -4,6 +4,7 @@ Created on Wed Apr  8 09:54:42 2020
 
 @author: Carlos CJ
 """
+# La funcion a_power_b sirve para realizar una potencia sin utilizar math ni **
 def a_power_b(a,b):
     Potencia = a
     if b == 1 :
@@ -12,6 +13,11 @@ def a_power_b(a,b):
         for r in range(2,(b+1)):
             Potencia = (Potencia * a)
         print("la potencia es  ", str(Potencia))
-a = int(input("Ingrese el valor del numero que va en a"))
-b = int(input("Ingrese el valor del numero que va en b"))
-print(a_power_b(a,b))
+def bucle():
+    a = 1
+    while a != 0 :
+        a = int(input("Ingrese el valor del numero que va en a :"))
+        b = int(input("Ingrese el valor del numero que va en b :"))
+        print(a_power_b(a,b))
+    print("termino")
+print(bucle())
